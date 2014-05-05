d48a28bd3a37e6498d6410a1ea33744dac7713f2
Faith Gleason <Faith@Faith-Gleasons-MacBook-Pro.local>
2014-05-05 12:08:58 -0700

    Initial Commit

---
+++ Alarm/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Alarm
+//
+//  Created by Faith Gleason on 5/5/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Alarm/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Alarm
+//
+//  Created by Faith Gleason on 5/5/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
